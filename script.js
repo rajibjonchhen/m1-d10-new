801ff4a93924b3f373605f42762e09fd3fa7d4de
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-11-05 19:05:52 +0000

    javascript object created and deleted 1properties

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = 'John'
+let y = 'Doe'
+console.log('John<>Doe')
+
+let profile = {
+name: 'Loreal', surname:'Ipsum', email:'loreal@ipsum.com'    
+}
+
+delete profile.email;
+console.log(profile)

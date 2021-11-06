58d15f286203958b45566d49cf99e443311f6018
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-11-06 00:00:05 +0000

    array of ten string and print it

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ name: 'Loreal', surname:'Ipsum', email:'loreal@ipsum.com'
 
 delete profile.email;
 console.log(profile)
+
+
+let js = ['datatypes','primitive ','numbers','string','boolean','undefined','null','function','array','object']
+for(let i = 0; i< 10; i++){
+    console.log(js[i])
+}

ed27175e044b7fb591e0f8c53c9dcfd544cbad71
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-11-06 00:16:57 +0000

    array 100 random numbers and finding biggest and the smallest

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ console.log(profile)
 let js = ['datatypes','primitive ','numbers','string','boolean','undefined','null','function','array','object']
 for(let i = 0; i< 10; i++){
     console.log(js[i])
+
 }
+
+let arrayOfRandomNumber = []
+for(let i =0; i < 100; i++){
+    let randomNumber = Math.floor(Math.random()*100)
+    arrayOfRandomNumber.push(randomNumber)
+}
+console.log(arrayOfRandomNumber)
+
+let biggestNumber = arrayOfRandomNumber[0]
+let smallestNumber =arrayOfRandomNumber[0]
+for(let i = 1; i < arrayOfRandomNumber.length; i++){
+    if(biggestNumber < arrayOfRandomNumber[i])
+        biggestNumber = arrayOfRandomNumber[i]
+}
+console.log('Biggest Number --', biggestNumber)
+
+for(let i = 1; i < arrayOfRandomNumber.length; i++){
+    if(smallestNumber > arrayOfRandomNumber[i])
+        biggestNumber = arrayOfRandomNumber[i]
+}
+console.log('Smallest Number --', smallestNumber)
